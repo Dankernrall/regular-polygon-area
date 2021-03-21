cb2ba35d2ecc0e6d5546ee7242a18dd2200f11bf
Никита <grand.marchello@gmail.com>
2021-03-21 21:19:32 +0400

    Решение задания 1

--- src/main/java/RegularPolygon.java
+++ src/main/java/RegularPolygon.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 
 public class RegularPolygon {
 
     public static void main(String[] args) {
-        //write your code here
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        double side = in.nextDouble();
+        if (n > 2 & side > 1) {
+            System.out.println(area(n, side));
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+
+    public static double area(int n, double side) {
+        double S = (n * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / n));
+        return S;
     }
 }
+
